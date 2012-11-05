cfd999fa9df417b16f776334c55bf376b4c88f48
Rakesh <rakesh.impinge@gmail.com>
2012-11-05 12:15:18 +0530

    tester

---
+++ test/TesterClass.h
@@ -0,0 +1,13 @@
+//
+//  TesterClass.h
+//  test
+//
+//  Created by Tarun Khosla on 05/11/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TesterClass : NSObject
+
+@end
---
+++ test/TesterClass.m
@@ -0,0 +1,13 @@
+//
+//  TesterClass.m
+//  test
+//
+//  Created by Tarun Khosla on 05/11/12.
+//
+//
+
+#import "TesterClass.h"
+
+@implementation TesterClass
+
+@end
